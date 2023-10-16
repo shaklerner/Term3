b141ee9d0c2d60ff30a6a9e17131e0d388a60b5a
Shak Shar <shakib.sharifian@edu.sait.ca>
2023-10-16 12:45:56 -0600

    Booking GUI changes

--- src/main/java/com/example/travelexpertsadministrator/AddEditBookingController.java
+++ src/main/java/com/example/travelexpertsadministrator/AddEditBookingController.java
@@ -9,66 +9,97 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-
 public class AddEditBookingController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
+    //initialize Package
+    public enum Mode {
+        ADD, EDIT
+    }
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+    private AddEditBookingController.Mode currentMode;
+    private Booking currentBooking;
 
-    @FXML // fx:id="btnAdd"
-    private Button btnAdd; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btnCancel"
-    private Button btnCancel; // Value injected by FXMLLoader
+        @FXML // ResourceBundle that was given to the FXMLLoader
+        private ResourceBundle resources;
 
-    @FXML // fx:id="cbPackageId"
-    private ComboBox<?> cbPackageId; // Value injected by FXMLLoader
+        @FXML // URL location of the FXML file that was given to the FXMLLoader
+        private URL location;
 
-    @FXML // fx:id="cbTavelType"
-    private ComboBox<?> cbTavelType; // Value injected by FXMLLoader
+        @FXML // fx:id="btnAdd"
+        private Button btnAdd; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tfBookingCount"
-    private TextField tfBookingCount; // Value injected by FXMLLoader
+        @FXML // fx:id="btnCancel"
+        private Button btnCancel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tfBookingDate"
-    private TextField tfBookingDate; // Value injected by FXMLLoader
+        @FXML // fx:id="cbCustomerID"
+        private ChoiceBox<?> cbCustomerID; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tfBookingId"
-    private TextField tfBookingId; // Value injected by FXMLLoader
+        @FXML // fx:id="cbPackageId"
+        private ComboBox<?> cbPackageId; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tfBookingNo"
-    private TextField tfBookingNo; // Value injected by FXMLLoader
+        @FXML // fx:id="cbTavelType"
+        private ComboBox<?> cbTavelType; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tfCustomerId"
-    private TextField tfCustomerId; // Value injected by FXMLLoader
+        @FXML // fx:id="dpBookingDate"
+        private DatePicker dpBookingDate; // Value injected by FXMLLoader
 
-    @FXML
-    void btnAddBookingClicked(ActionEvent event) {
+        @FXML // fx:id="tfBookingCount"
+        private TextField tfBookingCount; // Value injected by FXMLLoader
+
+        @FXML // fx:id="tfBookingId"
+        private TextField tfBookingId; // Value injected by FXMLLoader
+
+        @FXML // fx:id="tfBookingNo"
+        private TextField tfBookingNo; // Value injected by FXMLLoader
+
+        @FXML
+        void btnAddBookingClicked(ActionEvent event) {
+
+        }
+
+        @FXML
+        void btnCancelBookingCilcked(ActionEvent event) {
+
+        }
+
+        @FXML // This method is called by the FXMLLoader when initialization is complete
+        void initialize() {
+            assert btnAdd != null : "fx:id=\"btnAdd\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert cbCustomerID != null : "fx:id=\"cbCustomerID\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert cbPackageId != null : "fx:id=\"cbPackageId\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert cbTavelType != null : "fx:id=\"cbTavelType\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert dpBookingDate != null : "fx:id=\"dpBookingDate\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert tfBookingCount != null : "fx:id=\"tfBookingCount\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert tfBookingId != null : "fx:id=\"tfBookingId\" was not injected: check your FXML file 'Booking-view.fxml'.";
+            assert tfBookingNo != null : "fx:id=\"tfBookingNo\" was not injected: check your FXML file 'Booking-view.fxml'.";
+
+        }
+
+    public void setMainController(MainController mainController) {
+        // Intentionally left empty if not used elsewhere.
+    }
+
+    public void setModeAndData(AddEditBookingController.Mode mode, Booking booking) {
+        this.currentMode = mode;
+        this.currentBooking = booking;
+
+        if (mode == AddEditBookingController.Mode.EDIT && booking != null) {
+            btnAdd.setText("Edit");
+            populateFields(booking);
+        }
+    }
+
+    private void populateFields(Booking booking) {
+        tfBookingId.setEditable(false);
+        tfBookingId.setText(booking.getBookingId()+"");
 
     }
 
-    @FXML
-    void btnCancelBookingCilcked(ActionEvent event) {
-
-    }
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert btnAdd != null : "fx:id=\"btnAdd\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert cbPackageId != null : "fx:id=\"cbPackageId\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert cbTavelType != null : "fx:id=\"cbTavelType\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert tfBookingCount != null : "fx:id=\"tfBookingCount\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert tfBookingDate != null : "fx:id=\"tfBookingDate\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert tfBookingId != null : "fx:id=\"tfBookingId\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert tfBookingNo != null : "fx:id=\"tfBookingNo\" was not injected: check your FXML file 'Booking-view.fxml'.";
-        assert tfCustomerId != null : "fx:id=\"tfCustomerId\" was not injected: check your FXML file 'Booking-view.fxml'.";
-
-    }
 
 }
